0d17d882c55a2a53c76c790d0fc5bb27cf485055
Marcin Wierzbiński <marcin119a@gmail.com>
2020-12-23 13:40:05 +0100

    Create dpll.py

---
+++ dpll.py
@@ -0,0 +1,23 @@
+def dpll(cnf, assignments={}):
+ 
+    if len(cnf) == 0:
+        return True, assignments
+ 
+    if any([len(c)==0 for c in cnf]):
+        return False, None
+    
+    l = __select_literal(cnf)
+ 
+    new_cnf = [c for c in cnf if (l, True) not in c]
+    new_cnf = [c.difference({(l, False)}) for c in new_cnf]
+    sat, vals = dpll(new_cnf, {**assignments, **{l: True}})
+    if sat:
+        return sat, vals
+         
+    new_cnf = [c for c in cnf if (l, False) not in c]
+    new_cnf = [c.difference({(l, True)}) for c in new_cnf]
+    sat, vals = dpll(new_cnf, {**assignments, **{l: False}})
+    if sat:
+        return sat, vals
+ 
+    return False, None
